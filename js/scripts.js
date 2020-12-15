e4e2f4c50d8bf213c311bd097aa61d43b4ed0e3a
AnthonyCarpentier <65316248+AnthonyCarpentier@users.noreply.github.com>
2020-12-15 15:38:39 -0800

    Add files via upload
    I kinda just banged this one out, so this is the finished version(not very fancy looking but it works)

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+// This controls the amount of pixels for the width/height
+let pixelCountWidth = 16;
+
+let container = document.querySelector(`#container`);
+
+let wrapper = document.querySelector(`.wrapper`);
+wrapper.style.gridTemplateColumns = `repeat( `+ pixelCountWidth + `, 1fr)`;
+wrapper.style.gridTemplateRows = `repeat( `+ pixelCountWidth + `, 1fr)`;
+
+
+
+function drawPlayableArea (pixelCountWidth) {
+    for (i = 0; i < (pixelCountWidth * pixelCountWidth); i++) {
+        let drawablePixel = document.createElement(`div`);
+        drawablePixel.classList.add(`drawablePixel`);
+
+        container.appendChild(drawablePixel);
+    }
+
+    let drawablePixels = container.querySelectorAll(`div`);
+    drawablePixels.forEach(drawablePixel => { 
+        drawablePixel.addEventListener(`mouseover`, function () {
+            drawablePixel.style.backgroundColor = `white`;
+    })
+});
+}
+
+function resetPlayableArea () {
+    for (i = 0; i < (pixelCountWidth * pixelCountWidth); i++) {
+        let drawablePixels = container.querySelectorAll(`div`);
+    
+        drawablePixels.forEach(drawablePixel => { 
+        drawablePixel.remove();
+    });
+    }
+}
+
+drawPlayableArea(pixelCountWidth);
+
+
+
+let btn = document.querySelector(`#clearBtn`);
+btn.addEventListener(`click`, resetBoard);
+
+function resetBoard() {
+    let drawablePixels = container.querySelectorAll(`div`);
+    
+    drawablePixels.forEach(drawablePixel => { 
+        drawablePixel.style.backgroundColor = `black`;
+    });
+
+    let newPixelCountWidth = prompt(`Please enter the numeric value of pixels you want per side(default is 16, max is 100)`);
+    resetPlayableArea();
+    drawPlayableArea(newPixelCountWidth);
+    pixelCountWidth = newPixelCountWidth;
+
+    let wrapper = document.querySelector(`.wrapper`);
+    wrapper.style.gridTemplateColumns = `repeat( `+ pixelCountWidth + `, 1fr)`;
+    wrapper.style.gridTemplateRows = `repeat( `+ pixelCountWidth + `, 1fr)`;
+}
